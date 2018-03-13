c104928d7e3036c0ce279f7481dc4530c0fa2fa9
Terry Cai <gidcai@gmail.com>
2018-03-13 16:14:53 +0800

    support urlrewrite

--- server.js
+++ server.js
@@ -1,19 +1,25 @@
 const express = require('express')
 const app = express()
+const path = require('path')
+const fs = require('fs')
 
 app.use(express.static('./dist'))
 
 app.get('*', function(req, res, next) {
   // hidden html suffix
   // Inspired https://github.com/kapouer/express-urlrewrite
-  if(!/(assets|\.(
-     png|jpe?g|gif|svg
-    |woff2?|eot|ttf|otf
-    |mp4|webm|ogg|mp3|wav|flac|aac
-    |js|css|html)(\?.*)?$)/.test(req.path)) {
-      req.url = req.originalUrl = req.url + '.html';
+  if (!/(assets|\.(png|jpe?g|gif|svg|woff2?|eot|ttf|otf|mp4|webm|ogg|mp3|wav|flac|aac|js|css|html)(\?.*)?$)/.test(req.path)) {
+    req.url = req.originalUrl = req.url + '.html'
+    var file = path.join(__dirname, './dist', req.url)
+    if(fs.existsSync(file)) {
+      res.setHeader("Content-Type", "text/html");
+      let fileTpl = fs.readFileSync(file)
+      res.send(fileTpl)
+    } else {
+      next()
+    }
   }
-  next();
+  next()
 });
 
 app.listen(8090)
